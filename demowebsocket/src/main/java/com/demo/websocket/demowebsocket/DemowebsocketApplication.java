9fe1f8d76b3b3308ee470a25ed205ea10142aa8e
Mauricio <mauricioquinomontano@hotmail.com>
2022-04-10 13:33:05 -0500

    firstOne

---
+++ demowebsocket/src/main/java/com/demo/websocket/demowebsocket/DemowebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.demo.websocket.demowebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebsocketApplication.class, args);
+	}
+
+}
---
+++ demowebsocket/src/test/java/com/demo/websocket/demowebsocket/DemowebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.websocket.demowebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
